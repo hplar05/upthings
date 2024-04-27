0a1390aede544ec8faede286140ca095cbe13839
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-04-28 00:06:37 +0800

    fear: install shadcn ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,18 +5,18 @@ import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
   return (
-    <header className="flex item-center justify-between p-3">
-      <Link href={"/"}>
-        <h1 className="font font-bold text-xl ml-4">
+    <header className="flex item-center justify-between p-4">
+      <Link className="space-x-2" href={"/"}>
+        <h1 className="font font-bold text-xl">
           <span className="text-[#3A80EA]">Up</span>
           <span className="text-black">Things</span>
         </h1>
       </Link>
-      <div className="mr-4 flex items-center">
+      <div className="px-5 flex items-center space-x-2">
         <UserButton afterSignOutUrl="/" />
 
         <SignedOut>
-          <SignInButton afterSignInUrl="/dashboard" mode="modal" />
+          <SignInButton mode="modal" />
         </SignedOut>
       </div>
     </header>
