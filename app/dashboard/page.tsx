3f49bd01ced900812400a38e6b673e6e2ef93d6f
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-04-29 07:42:24 +0800

    feat: drag and drop items

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,15 @@
+import Dropzone from "@/components/Dropzone";
+import { auth } from "@clerk/nextjs/server";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { userId } = auth();
+  return (
+    <main className="h-[100vh]">
+      <div>
+        <Dropzone />
+      </div>
+    </main>
+  );
 };
 
 export default Dashboard;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <footer className="bg-transparent dark:text-white text-black py-4 text-center ">
       <div className="container mx-auto">
-        <p>&copy; 2024 UpThings. All rights reserved.</p>
+        <p>
+          &copy; 2024 UpThings Created by Hplar/Morpheus. All rights reserved.
+        </p>
       </div>
     </footer>
   );
